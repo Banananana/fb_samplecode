9fe4070fa2511dd4b3167e925bf319d213ed7852
Sidney <mac@Macteki-MacBook-Air-7.local>
2013-12-24 00:08:59 +0800

    Initial Commit

---
+++ fb sdk test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fb sdk test
+//
+//  Created by Sidney on 2013/12/24.
+//  Copyright (c) 2013年 Sidney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
